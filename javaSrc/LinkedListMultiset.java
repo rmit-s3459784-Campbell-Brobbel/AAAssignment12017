8a4769b1801fe82a85408c473284c9558228ec70
Nyuol <s3243720@student.rmit.edu.au>
2017-08-11 23:59:22 +1000

    Commited some more codes

--- javaSrc/LinkedListMultiset.java
+++ javaSrc/LinkedListMultiset.java
@@ -3,37 +3,135 @@ import java.util.*;
 
 public class LinkedListMultiset<T> extends Multiset<T>
 	// changes made s3243720
+    // Go to make changes soon
+	protected Node mHead;
+		protected int mLength
 {
-	public LinkedListMultiset() {
+	public LinkedListMultiset(){
 		// Implement me!
-	} // end of LinkedListMultiset()
-	
-	
-	public void add(T item) {
-		// Implement me!
-	} // end of add()
-	
+	 // end of LinkedListMultiset()
+		 mHead = null;
+		 mLength = 0;
+		)
+	public void add(T item) (
+			Node newNode = new Node((String) item);
+		if (mHead == null)
+		mHead = newNode;
+		else {
+		Node currNode = mHead;
+		Node parentNode = null;
+		while (currNode != null) {
+		if (currNode.getValue().
+		equals(newNode.getValue())) {
+		currNode.addCounter();
+		return;
+		}
+		parentNode = currNode;
+		currNode = currNode.getNext();
+		}
+		parentNode.setNext(newNode);
+		}
+		mLength++;
+		}
 	
 	public int search(T item) {
-		// Implement me!		
-		
-		// default return, please override when you implement this method
+		Node currNode = mHead;
+
+		while (currNode != null){
+		if(currNode.getValue().equals((String)item)){
+		return currNode.getCounter();
+		}
+		currNode=currNode.getNext();
+		}
+
 		return 0;
-	} // end of add()
-	
-	
-	public void removeOne(T item) {
-		// Implement me!
-	} // end of removeOne()
-	
-	
+		}
+
+public void removeOne(T item) {
+		Node currNode = mHead;
+		Node lastNode = null;
+
+		while (currNode != null) {
+		if (currNode.getValue().equals((String) item)) {
+		currNode.minusCounter();
+		if (currNode.getCounter() == 0) {
+		if (currNode == mHead)
+		mHead = currNode.getNext();
+		else
+		lastNode.setNext
+		(currNode.getNext());
+		mLength--;
+		}
+		return;
+		}
+		lastNode = currNode;
+		currNode = currNode.getNext();
+		}
+		}
 	public void removeAll(T item) {
-		// Implement me!
-	} // end of removeAll()
+		Node currNode = mHead;
+		Node lastNode = null;
+
+		while (currNode != null) {
+		if (currNode.getValue().equals((String) item)) {
+		if (currNode == mHead)
+		mHead = currNode.getNext();
+		else
+		lastNode.setNext(currNode.getNext());
+		mLength--;
+		return;
+		}
+		lastNode = currNode;
+		currNode = currNode.getNext();
+		}
+		}
 	
-	
-	public void print(PrintStream out) {
-		// Implement me!
-	} // end of print()
+	public void print(PrintStream out)  Node currNode = mHead;
+
+		while (currNode != null) {
+		out.printf("%s | %d\n", currNode.getValue()
+		, currNode.getCounter());
+		currNode = currNode.getNext();
+		}
+		}
+private class Node {
+	protected String mValue;
+	protected Node mNext;
+
+	int counter;
+
+	public Node(String value) {
+		mValue = value;
+		mNext = null;
+		counter = 1;
+	}
+
+	public void addCounter() {
+		counter++;
+	}
+
+	public void minusCounter() {
+		counter--;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public String getValue() {
+		return mValue;
+	}
+
+	public Node getNext() {
+		return mNext;
+	}
+
+	public void setValue(String value) {
+		mValue = value;
+	}
+
+	public void setNext(Node next) {
+		mNext = next;
+	}
 	
 } // end of class LinkedListMultiset
