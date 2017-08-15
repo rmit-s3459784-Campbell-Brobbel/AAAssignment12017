295019500562510c741fec0a9769ea513cab4357
Nyuol <s3243720@student.rmit.edu.au>
2017-08-15 18:00:50 +1000

    error:(141,2) java: reached end of file while parsing, is fixed

--- javaSrc/LinkedListMultiset.java
+++ javaSrc/LinkedListMultiset.java
@@ -1,139 +1,145 @@
 import java.io.PrintStream;
 import java.util.*;
 
-public class LinkedListMultiset<T> extends Multiset<T>{
-        // changes made s3243720
-        // Go to make changes soon
-        protected Node mHead;
-        protected int mLength;
+public class LinkedListMultiset<T> extends Multiset<T> {
+    // changes made s3243720
+    // Go to make changes soon
+    protected Node mHead;
+    protected int mLength;
 
-   // define the head and the length of the linkedlist
-public LinkedListMultiset(){
-        // Implement me!
-        // end of LinkedListMultiset()
-        mHead=null;
-        mLength=0;
-        }
-public void add(T item){
+    // define the head and the length of the linkedlist
+    public LinkedListMultiset() {
+        /*
+        Implement me!
+        end of LinkedListMultiset()
+        */
+        mHead = null;
+        mLength = 0;
+    }
+
+    public void add(T item) {
         // Create a new node object with intial head and length
-        Node newNode=new Node((String)item);
-        if(mHead==null)
-        mHead=newNode;
-        else{
-        Node currentNode=mHead;
-        Node parentNode=null;
-        while(currentNode!=null){
-        if(currentNode.getValue().
-        equals(newNode.getValue())){
-        currentNode.addCounter();
-        return;
-        }
-        parentNode=currentNode;
-        currentNode=currentNode.getNext();
-        }
-        parentNode.setNext(newNode);
+        Node newNode = new Node((String) item);
+        if (mHead == null)
+            mHead = newNode;
+        else {
+            Node currentNode = mHead;
+            Node parentNode = null;
+            while (currentNode != null) {
+                if (currentNode.getValue().
+                        equals(newNode.getValue())) {
+                    currentNode.addCounter();
+                    return;
+                }
+                parentNode = currentNode;
+                currentNode = currentNode.getNext();
+            }
+            parentNode.setNext(newNode);
         }
         mLength++;
-}
+    }
 
-public int search(T item){
+    public int search(T item) {
         Node currentNode = mHead;
 
-        while(currentNode!=null){
-        if(currentNode.getValue().equals((String)item)){
-        return currentNode.getCounter();
-        }
-        currentNode=currentNode.getNext();
+        while (currentNode != null) {
+            if (currentNode.getValue().equals((String) item)) {
+                return currentNode.getCounter();
+            }
+            currentNode = currentNode.getNext();
         }
 
         return 0;
-        }
+    }
 
-public void removeOne(T item){
-        Node currentNode=mHead;
-        Node lastNode=null;
+    public void removeOne(T item) {
+        Node currentNode = mHead;
+        Node lastNode = null;
 
-        while(currentNode!=null){
-        if(currentNode.getValue().equals((String)item)){
-        currentNode.minusCounter();
-        if(currentNode.getCounter()==0){
-        if(currentNode==mHead)
-        mHead=currentNode.getNext();
-        else lastNode.setNext
-        (currentNode.getNext());
-        mLength--;
-        }
-        return;
-        }
-        lastNode=currentNode;
-        currentNode=currentNode.getNext();
-        }
-        }
-public void removeAll(T item){
-            Node currentNode=mHead;
-            Node lastNode=null;
-
-            while(currentNode!=null){
-            if(currentNode.getValue().equals((String)item)){
-            if(currentNode==mHead)
-            mHead=currentNode.getNext();
-            else lastNode.setNext(currentNode.getNext());
-            mLength--;
-            return;
+        while (currentNode != null) {
+            if (currentNode.getValue().equals((String) item)) {
+                currentNode.minusCounter();
+                if (currentNode.getCounter() == 0) {
+                    if (currentNode == mHead)
+                        mHead = currentNode.getNext();
+                    else lastNode.setNext
+                            (currentNode.getNext());
+                    mLength--;
+                }
+                return;
             }
-            lastNode=currentNode;
-            currentNode=currentNode.getNext();
-            }
-            }
-
-public void print(PrintStream out){
-        Node currentNode=mHead;
-
-        while(currentNode!=null){
-        out.printf("%s | %d\n",currentNode.getValue()
-        ,currentNode.getCounter());
-        currentNode=currentNode.getNext();
+            lastNode = currentNode;
+            currentNode = currentNode.getNext();
         }
+    }
+
+    public void removeAll(T item) {
+        Node currentNode = mHead;
+        Node lastNode = null;
+
+        while (currentNode != null) {
+            if (currentNode.getValue().equals((String) item)) {
+                if (currentNode == mHead)
+                    mHead = currentNode.getNext();
+                else lastNode.setNext(currentNode.getNext());
+                mLength--;
+                return;
+            }
+            lastNode = currentNode;
+            currentNode = currentNode.getNext();
         }
-// creating and inner private class to Node
-private class Node {
-    protected String mValue;
-    protected Node mNext;
-
-    int counter;
-
-    public Node(String value) {
-        mValue = value;
-        mNext = null;
-        counter = 1;
     }
 
-    public void addCounter() {
-        counter++;
+    public void print(PrintStream out) {
+        Node currentNode = mHead;
+
+        while (currentNode != null) {
+            out.printf("%s | %d\n", currentNode.getValue()
+                    , currentNode.getCounter());
+            currentNode = currentNode.getNext();
+        }
     }
 
-    public void minusCounter() {
-        counter--;
-    }
+    // creating and inner private class to Node
+    private class Node {
+        protected String mValue;
+        protected Node mNext;
 
-    public int getCounter() {
-        return counter;
-    }
+        int counter;
 
-    public String getValue() {
-        return mValue;
-    }
+        public Node(String value) {
+            mValue = value;
+            mNext = null;
+            counter = 1;
+        }
 
-    public Node getNext() {
-        return mNext;
-    }
+        public void addCounter() {
+            counter++;
+        }
 
-    public void setValue(String value) {
-        mValue = value;
-    }
+        public void minusCounter() {
+            counter--;
+        }
 
-    public void setNext(Node next) {
-        mNext = next;
-    }
+        public int getCounter() {
+            return counter;
+        }
 
-} // end of class LinkedListMultiset
+        public String getValue() {
+            return mValue;
+        }
+
+        public Node getNext() {
+            return mNext;
+        }
+
+        public void setValue(String value) {
+            mValue = value;
+        }
+
+        public void setNext(Node next) {
+            mNext = next;
+        }
+
+    }
+}// end of class LinkedListMultiset
